18f6f148a240cac035070d8b5c4b0e46eae9a1b3
Federico Polezzi <federico.polezzi@gmail.com>
2021-01-09 23:45:48 +0100

    rename

--- scripts/start.js
+++ scripts/start.js
@@ -24,7 +24,7 @@ var dictionaryImages = {};
 dictionaryImages["live"] = 
 [
     {
-        "file": "dsc0131.jpg",
+        "file": "_dsc0131.jpg",
         "description": "Luca - Cortona 2017",
         "imgtype": ""
     },
